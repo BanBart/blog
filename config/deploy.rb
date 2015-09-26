86e2a979366f0f6df05eeaf1876b81ced0ced4e5
Bartlomiej Banasik <bartlomiej.banasik@ananastar.com>
2015-09-26 23:06:29 +0200

    change number of workers for puma server

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url,        'git@github.com:BanBart/blog.git'
 set :application,     'blog'
 set :user,            'sakyhank'
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
