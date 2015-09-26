af05bb5a7b3ef9a9b1ae1eb199df5db7da0937db
Bartlomiej Banasik <bartlomiej.banasik@ananastar.com>
2015-09-26 17:28:29 +0200

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,7 +92,7 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start', 'db:configure'
+      before 'deploy:restart', 'puma:start'
       invoke 'deploy'
     end
   end
