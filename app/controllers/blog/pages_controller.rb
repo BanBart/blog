c136c28c545d1c9d6e465b449f112075f9b400ef
Bartlomiej Banasik <bartlomiej.banasik@ananastar.com>
2015-09-27 15:54:26 +0200

    add blog subdomain

---
+++ app/controllers/blog/pages_controller.rb
@@ -0,0 +1,6 @@
+module Blog
+	class Blog::PagesController < ApplicationController
+	  def index
+	  end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  scope module: :blog, constraints: { subdomain: 'blog' } do 
+    get '/' => 'pages#index'
+  end
+
   root 'main#index'
 end
